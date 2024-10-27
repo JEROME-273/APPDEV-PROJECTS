a1e8b3badef154a46858723c2778e472bf99cfd4
Carpio, John Ray M. <165807539+Johnray1404@users.noreply.github.com>
2024-10-27 11:41:07 +0800

    Add files via upload

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const userController = require('../controller/userController'); 
+const userController = require('../controller/mainController'); 
 const router = express.Router();
 
 // User routes
