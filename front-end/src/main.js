8873ccd8cdfa9e3ae2967f620b061e61ba1869f4
Paul Jerome A. Martinez <firedown1231@gmail.com>
2024-11-09 14:34:49 +0800

    deleting vue

--- front-end/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import '@fortawesome/fontawesome-free/css/all.css';
-
-
-createApp(App).use(router).mount('#app')
